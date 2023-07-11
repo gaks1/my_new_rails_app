c64c83f89a48f00837b005df71c5c08cc2ce7a00
isaacg <iwanyoike65@gmail.com>
2023-07-11 18:11:36 +0300

    Add redux store and add loading

---
+++ app/javascript/redux/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import greetingsReducer from "./greetings/greetingsSlice";
+
+const store = configureStore({
+    reducer: {
+        greetings: greetingsReducer,
+    },
+});
